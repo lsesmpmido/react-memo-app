6e6e1443d5125c2629db5141c43c8f08cb90ea91
r.kawano <r.kawano@genbasupport.com>
2025-02-20 14:52:11 +0900

    ログイン機能を追加

--- src/MemoList.js
+++ src/MemoList.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import MemoItem from "./MemoItem";
+import { SignInContext } from "./Context.js";
 
 function MemoList({ memos, onSelectMemo, onCreateMemo }) {
+  const isSignIn = useContext(SignInContext);
+
   return (
     <div className="frame">
       <h2>一覧</h2>
-      <button onClick={onCreateMemo}>+</button> {}
+      {isSignIn && <button onClick={onCreateMemo}>+</button>}
       <ul>
         {memos.map((memo) => (
           <MemoItem key={memo.id} memo={memo} onSelectMemo={onSelectMemo} />
